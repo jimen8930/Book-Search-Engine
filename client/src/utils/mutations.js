7b8428d54802424694c3e7a665f9caac71348eb5
jimen8930 <jimen8930@aol.com>
2023-08-31 21:36:50 -0400

    Created the queries and mutations files. I added the import for apollo and created export for the user to login and add a user.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,44 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          description
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          description
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`;
