0839af3cd42c1ba05d7d689bce2d681f372db154
jimen8930 <jimen8930@aol.com>
2023-08-31 22:05:41 -0400

    Created a mutation for the user to save a book to their saveBook collections.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -42,3 +42,21 @@ export const ADD_USER = gql`
     }
   }
 `;
+export const SAVE_BOOK = gql`
+mutation saveBook ($input: BookInput!) {
+    saveBook(input: $input) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    } 
+}
+`;
